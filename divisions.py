af01de381803c0843f3aaef367560411f8b9a1ad
Newix17 <luniversderic@gmail.com>
2022-01-12 17:11:51 +0100

    fini mais a ameliorer

--- divisions.py
+++ divisions.py
@@ -1,6 +1,3 @@
-# from main import *
-import re
-import csv
 
 non_admis=[]
 poussin=[]
@@ -16,25 +13,34 @@ with open("inscrits_total_v2.csv", "r") as f:
         ligne = [x for x in line[:-2].split(",")]
         if "Poussin" in ligne[2]:
             poussin.append(ligne[0])
-            poussin.append(ligne[1])
         if "Cadet" in ligne[2]:
             cadet.append(ligne[0])
-            cadet.append(ligne[1])
         if "Junior" in ligne[2]:
             junior.append(ligne[0])
-            junior.append(ligne[1])
         if "Semi-Pro" in ligne[2]:
             semipro.append(ligne[0])
-            semipro.append(ligne[1])
         if "Pro" in ligne[2]:
             pro.append(ligne[0])
-            pro.append(ligne[1])
         if "Non admis" in ligne[2]:
             non_admis.append(ligne[0])
-            non_admis.append(ligne[1])
         else:
             pass
 
-print("Poussin:",(poussin),"\n","Cadet:",(cadet),"\n","Junior:",(junior),"\n","Semi-Pro:",(semipro),"\n","Pro:",(pro),"\n","Non Admis:",(non_admis),"\n",sep="")
-
-
+print("La categorie POUSSIN contient:",(len(poussin)),"personne(s):")
+for i in poussin:
+    print(i)
+print("La categorie CADET contient:",(len(cadet)),"personne(s):")
+for i in cadet:
+    print(i)
+print("La categorie JUNIOR contient:",(len(junior)),"personne(s):")
+for i in junior:
+    print(i)
+print("La categorie SEMI-PRO contient:",(len(semipro)),"personne(s):")
+for i in semipro:
+    print(i)
+print("La categorie PRO contient:",(len(pro)),"personne(s):")
+for i in pro:
+    print(i)
+print("La categorie NON ADMISSIBLE contient:",(len(non_admis)),"personne(s):")
+for i in non_admis:
+    print(i)
